51f82880d88aebc3ef3645371c7d0d9eca05eb57
Erik de Lange <erikdelange@users.noreply.github.com>
2021-01-23 09:56:11 +0100

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -3,7 +3,7 @@ html = """<!DOCTYPE html>
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- meta http-equiv="refresh" content="15" -->
+        <meta http-equiv="refresh" content="15">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link rel="icon" href="data:;base64,=">
         <title> WiPy </title>
